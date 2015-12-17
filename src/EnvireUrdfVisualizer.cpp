6dd1bc7a9c0bc077e143b1959f51a261ac420e7e
Arne Böckmann <arne.boeckmann@dfki.de>
2015-12-17 15:56:07 +0100

    dummy arrow

--- src/EnvireUrdfVisualizer.cpp
+++ src/EnvireUrdfVisualizer.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <typeinfo>
 #include <envire_core/items/Item.hpp>
-//OSGVIZ_PLUGIN(EnvireUrdfVisualizer);
+#include <osg/Shape>
+#include <osg/ShapeDrawable>
 
 namespace envire { namespace visualizer {
   
@@ -11,18 +12,38 @@ OSGVIZ_PLUGIN(EnvireUrdfVisualizer);
 EnvireUrdfVisualizer::EnvireUrdfVisualizer(lib_manager::LibManager* pManager) :
   EnvireVizPlugin(pManager)
 {
-  supportedTypes.emplace_back(typeid(envire::core::Item<std::string>));
+  supportedTypes.emplace_back(typeid(envire::core::StringItem::Ptr));
 }
   
 osg::ref_ptr<osgviz::Object> EnvireUrdfVisualizer::createItemVisualization(const std::type_index& type, const core::ItemBase::Ptr item)
 {
-  std::cout << "CREATE" << std::endl;
-  return osg::ref_ptr<osgviz::Object>(nullptr);
+  osg::ref_ptr<osg::Geode> geode = new osg::Geode();
+
+  float coneheight = 0.3;
+  float coneoffset = -0.07; //whyever the center po is not really the center
+  float coneposz =  -(coneheight/2.0)+coneoffset;
+
+  osg::ref_ptr<osg::Cone> cone = new osg::Cone(osg::Vec3(0,0,coneposz),0.1,coneheight);
+
+  float arrowheight = 1;
+  float cylinderheight=arrowheight-coneheight;
+  float cylinderposz=-arrowheight/2.0  - coneheight/2.0;
+
+  osg::ref_ptr<osg::Cylinder> cylinder = new osg::Cylinder(osg::Vec3(0,0,cylinderposz),0.05,cylinderheight);
+
+
+  osg::ref_ptr<osg::ShapeDrawable> coneDrawable = new osg::ShapeDrawable(cone);
+  osg::ref_ptr<osg::ShapeDrawable> cylinderDrawable = new osg::ShapeDrawable(cylinder);
+
+  geode->addDrawable(coneDrawable);
+  geode->addDrawable(cylinderDrawable);
+  osg::ref_ptr<osgviz::Object> o(new osgviz::Object());
+  o->addChild(geode);
+  return o;
 }
 
 const std::vector<std::type_index>& EnvireUrdfVisualizer::getSupportedTypes()
-{
-  std::cout << "GET" << std::endl;
+{ 
   return supportedTypes;
 }
 
@@ -36,6 +57,4 @@ int EnvireUrdfVisualizer::getLibVersion() const
   return 0;
 }
 
-
-  
 }}
